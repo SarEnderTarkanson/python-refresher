ca4c0546ea817749595a5b2f1ed8da2f776c4a16
SarEnderTarkanson <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-20 13:42:36 +0200

    basic exercises in list comprehension

---
+++ 12_list_comprehensions/code.py
@@ -0,0 +1,17 @@
+# numbers = [1, 3, 5]
+# doubled = [x * 2 for x in numbers]
+
+# print(doubled)
+
+friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+
+starts_s = [friend for friend in friends if friend.startswith("S")]
+
+# for friend in friends:
+#     if friend.startswith("S"):
+#         starts_s.append(friend)
+        
+print(friends)
+print(starts_s)
+print(friends[1] is starts_s[0])
+print("friends: ", id(friends), "starts_s: ", id(starts_s))
