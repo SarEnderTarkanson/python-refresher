67c28f3323594720af55c4dc6a3695354b1371ee
SarEnderTarkanson <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-23 10:55:04 +0200

    added exercise for functions

---
+++ 15_functions/code.py
@@ -0,0 +1,18 @@
+# def hello():
+#     print("Hello!")
+    
+# hello()
+
+# def user_age_in_seconds():
+#     user_age = int(input("Enter your age: "))
+#     age_seconds = user_age * 365 * 24 * 60 * 60
+#     print(f"Your age in seconds is {age_seconds}.")
+    
+# user_age_in_seconds()
+
+friends = []
+def add_friend():
+    friends.append("Rolf")
+    print(friends)
+    
+add_friend()
