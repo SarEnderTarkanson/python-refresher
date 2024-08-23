8289f95eb4c73ea61e7829a502f7c00835c10f13
SarEnderTarkanson <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-23 11:11:53 +0200

    added exercise for arguments and parameters to functions

---
+++ 16_function_arguments_parameters/code.py
@@ -0,0 +1,23 @@
+# def add(x, y):
+#     result = x + y
+#     print(result)
+
+# add(5,7)
+
+# def say_hello():
+#     print("Hello!")
+    
+# say_hello()
+
+# def say_hello(name, surname):
+#     print(f"Hello, {name} {surname}")
+    
+# say_hello("Alpy", "Tarkanson")
+
+def divide(dividend, divisor):
+    if divisor != 0:
+        print(dividend / divisor)
+    else:
+        print("You Fool!")
+        
+divide(divisor=15,dividend=0)
